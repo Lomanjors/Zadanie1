80c8d8f04e44b7aebcb05e8f5ce7c7f378096cb2
Lomanjors <Lomanjors555995@gmail.com>
2025-03-02 08:56:57 +0300

    Update index.php

--- Zadanie2/index.php
+++ Zadanie2/index.php
@@ -13,3 +13,16 @@ $userService = new UserService();
 $sortedByUsernameAsc = $userService->sortByUsername($users, 'asc');
 
 $sortedByBirthdayDesc = $userService->sortByBirthday($users, 'desc');
+
+echo "Сортировка по имени (возрастание):\n";
+foreach ($sortedByUsernameAsc as $user) {
+    echo $user->getUsername() . "\n";
+}
+
+$sortedByBirthdayDesc = $userService->sortByBirthday($users, 'desc');
+
+echo "\nСортировка по дате рождения (убывание):\n";
+foreach ($sortedByBirthdayDesc as $user) {
+    echo $user->getUsername() . " - " . $user->getBirthday()->format('Y-m-d') . "\n";
+}
+?>
