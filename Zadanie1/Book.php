c345468bbfb9e7f2c3fdc50ede196960ea4e3347
Lomanjors <Lomanjors555995@gmail.com>
2025-03-04 07:57:48 +0300

    Update Book.php

--- Zadanie1/Book.php
+++ Zadanie1/Book.php
@@ -4,25 +4,30 @@ class Book {
     private $author;
     private $isAvailable;
 
-    public function __construct($title, $author) {
+    public function __construct($title, $author) 
+    {
         $this->title = $title;
         $this->author = $author;
         $this->isAvailable = true;
     }
 
-    public function getTitle() {
+    public function getTitle() :string
+    {
         return $this->title;
     }
 
-    public function getAuthor() {
+    public function getAuthor() :string
+    {
         return $this->author;
     }
 
-    public function isAvailable() {
+    public function isAvailable() :bool
+    {
         return $this->isAvailable;
     }
 
-    public function setAvailable($availability) {
+    public function setAvailable($availability): void 
+    {
         $this->isAvailable = $availability;
     }
 }
