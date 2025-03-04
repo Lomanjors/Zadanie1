eff713eb4319bcb4417053e0028ee9560908143c
Lomanjors <Lomanjors555995@gmail.com>
2025-03-04 08:00:06 +0300

    Update User.php

--- Zadanie2/User.php
+++ Zadanie2/User.php
@@ -1,25 +1,28 @@
 <?php
-class User {    
-    private $username;
-    private $password;
-    private $birthday;
+require_once "User.php";
+require_once "UserService.php";
 
-    public function __construct($username, $password, $birthday) {
-        $this->username = $username;
-        $this->password = $password;
-        $this->birthday = new DateTime($birthday);
-    }
+$user1 = new User("Alice", "password123", "1990-05-01");
+$user2 = new User("Bob", "password456", "1985-10-15");
+$user3 = new User("Charlie", "password789", "1995-12-25");
 
-    public function getUsername() {
-        return $this->username;
-    }
+$users = [$user1, $user2, $user3];
 
-    public function getPassword() {
-        return $this->password;
-    }
+$userService = new UserService();
 
-    public function getBirthday() {
-        return $this->birthday;
-    }
+$sortedByUsernameAsc = $userService->sortByUsername($users, 'asc');
+
+$sortedByBirthdayDesc = $userService->sortByBirthday($users, 'desc');
+
+echo "Сортировка по имени (возрастание):\n";
+foreach ($sortedByUsernameAsc as $user) {
+    echo $user->getUsername() . "\n";
 }
 
+$sortedByBirthdayDesc = $userService->sortByBirthday($users, 'desc');
+
+echo "\nСортировка по дате рождения (убывание):\n";
+foreach ($sortedByBirthdayDesc as $user) {
+    echo $user->getUsername() . " - " . $user->getBirthday()->format('Y-m-d') . "\n";
+}
+?>
