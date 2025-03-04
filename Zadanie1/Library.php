7d76e0c125df61ecc21d934849367a1858f77896
Lomanjors <Lomanjors555995@gmail.com>
2025-03-04 07:58:07 +0300

    Update Library.php

--- Zadanie1/Library.php
+++ Zadanie1/Library.php
@@ -3,15 +3,18 @@ class Library {
     private $books = [];
     private $users = [];
 
-    public function addBook($book) {
+    public function addBook($book) :void
+    {
         $this->books[] = $book;
     }
 
-    public function registerUser($user) {
+    public function registerUser($user): void  
+    {
         $this->users[] = $user;
     }
 
-    public function findBookByTitle($title) {
+    public function findBookByTitle($title) :mixed
+    {
         foreach ($this->books as $book) {
             if ($book->getTitle() === $title) {
                 return $book;
@@ -20,7 +23,8 @@ class Library {
         return null;
     }
 
-    public function findUserById($userID) {
+    public function findUserById($userID): mixed 
+    {
         foreach ($this->users as $user) {
             if ($user->userID === $userID) {
                 return $user;
